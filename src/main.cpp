b0b75978bcd95ef4642e7ab99f07bb0a80ffcf99
mxmanseven <karlhess@msn.com>
2018-05-03 21:41:13 -0700

    eeprom read and write worked with out lib. i2c eepromt is device address 0x57.

--- src/main.cpp
+++ src/main.cpp
@@ -29,37 +29,82 @@ void setup()
 
     Serial.begin(9600);
 
+    while(!Serial) {}
+
     Wire.begin(
         I2C_MASTER, 
         0x00, 
         I2C_PINS_16_17, 
-        I2C_PULLUP_INT, 
+        I2C_PULLUP_EXT, 
         400000);
 
     //Wire.begin();
 
     // disable internal pull up which is set by default in Wire.h
-    // digitalWrite(SDA, 0);
-    // digitalWrite(SCL, 0);
+    Serial.println("sda: " +  String(SDA));
+    Serial.println("sdl: " +  String(SCL));
 
-    //eep.begin(extEEPROM::twiClock400kHz);
 
-    // write
+    // digitalWrite(18, 1);
+    // digitalWrite(19, 1);
+
     delay(100);
-    //eep.write(0, 13);
+
+// write
+    eep.begin(extEEPROM::twiClock400kHz);
+    eep.write(10, 13);
 
     // read
 }
 
-void loop() {
-    delay(1000);
+void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data ) {
+    int rdata = data;
+    Wire.beginTransmission(deviceaddress);
+    Wire.write((int)(eeaddress >> 8)); // MSB
+    Wire.write((int)(eeaddress & 0xFF)); // LSB
+    Wire.write(rdata);
+    Wire.endTransmission();
+}
 
+byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress ) {
+    byte rdata = 0xFF;
+    Wire.beginTransmission(deviceaddress);
+    Wire.write((int)(eeaddress >> 8)); // MSB
+    Wire.write((int)(eeaddress & 0xFF)); // LSB
+    Wire.endTransmission();
+    Wire.requestFrom(deviceaddress,1);
+    if (Wire.available()) rdata = Wire.read();
+    return rdata;
+}
+
+
+// 0x57 works!
+int deviceaAddr = 0;
+
+void loop() {
+    delay(500);
+
+    Serial.println("deviceAddr: " + String(deviceaAddr));
+
+    i2c_eeprom_write_byte(deviceaAddr, 0, 12);
+    delay(100);
+
+    int readValue = 0;
+    readValue = i2c_eeprom_read_byte(deviceaAddr, 0);
+    Serial.println("eeprom " + String(readValue));
+
+    if(readValue != 0xff)
+    {
+        Serial.println("i2c worked addr:" + String(deviceaAddr));
+        while(1) {}
+    }
 
     // int readValue = 0;
-    // readValue = eep.read(0);
-    // Serial.println(String(readValue));
-
+    // readValue = eep.read(10);
+    // Serial.println("eeprom " + String(readValue));
 
     int unixTime = timeKnh.getUnitxTime();
     Serial.println(String(unixTime)); 
+
+    deviceaAddr++;
 }
